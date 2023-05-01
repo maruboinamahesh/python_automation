0e883681ae88d49e1a210293f2e0c54d38af1d53
maruboinamahesh <132250184+maruboinamahesh@users.noreply.github.com>
2023-05-01 11:32:56 +0530

    data types

---
+++ 1_number (1).py
@@ -0,0 +1,30 @@
+'''
+1. Number data type is used to store integer values(0-9).
+
+    Example :-  N = 123
+
+    1. 'N' is a Variable Name
+    2. '=' is a assignment operator
+    3. '123' is a value
+    4. 'N' Data type is number(int)
+
+'''
+
+# To Create a variable with Number data type
+n = 123
+
+# To Print on console
+print(n)
+
+# To Update and Append
+n = 555
+
+# To delete a Varable
+del n
+
+
+
+
+
+
+
---
+++ 2_string (1).py
@@ -0,0 +1,28 @@
+'''
+1. Using string we can store any value but it should be in single quotes or double quotes.
+
+'''
+
+# To define a string variable
+name = 'sri 123$#%'
+
+# If string contains single quote 
+name1 = " I don't know sriram"
+
+# If string contains double quote 
+name2 = ' I know "sriram"'
+
+# If string contains single quote and double quote 
+name3 = ''' I don't know "sriram"'''
+
+
+# To update
+name = 'kumar'
+print(name)
+
+# To Delete 
+del name 
+
+# To Print on console
+print(' Name is : ', name)
+
